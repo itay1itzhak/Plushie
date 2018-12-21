6b1078596fba9394c1bfca92353b65c8f9601b9c
itay1itzhak <itay1itzhak@gmail.com>
2018-12-21 16:25:36 +0200

    init

---
+++ Plushie.java
@@ -0,0 +1,23 @@
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import javax.swing.JFrame;
+
+public class Plushie extends JFrame{
+
+	static Dimension ScreenSize = Toolkit.getDefaultToolkit().getScreenSize(); 
+	static JFrame F = new Plushie();
+	Screen ScreenObject = new Screen();
+	
+	public Plushie()
+	{
+		add(ScreenObject);
+		setUndecorated(true);
+		setSize(ScreenSize);
+		setVisible(true);
+	}
+	
+	public static void main(String[] args)
+	{
+		
+	}
+}
---
+++ Polygon2D.java
@@ -0,0 +1,27 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Polygon;
+
+
+public class Polygon2D {
+	Polygon P;
+	Color c;
+	double AvgDist = 0;
+	
+	public Polygon2D(double[] x, double[] y, Color c)
+	{
+		Screen.NumberOfPolygons++;
+		P = new Polygon();
+		for(int i = 0; i < x.length; i++)
+			P.addPoint((int)x[i], (int)y[i]);
+		this.c = c;
+	}
+	
+	void drawPolygon(Graphics g)
+	{
+		g.setColor(c);
+		g.fillPolygon(P);
+		g.setColor(Color.black);
+		g.drawPolygon(P);
+	}
+}
