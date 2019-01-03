816f0fd1fe15b647be50506954ae65ba3526f0d7
itay itzhak <Itayit>
2019-01-03 22:51:13 +0200

    inflate(1)

--- Simulation.java
+++ Simulation.java
@@ -6,13 +6,22 @@ import java.util.Arrays;
 public class Simulation {
 
 	
-//	//TODO
-//	public void inflate(Polygon3D poly3d) {
-//	
-//		poly3d =  Polygon3D();
-//
-//	}
-//	
+	
+	public void inflate(Polygon3D[] poly3d, int patchNum, int numOfPatches) {
+		for (int i=0; i<poly3d[patchNum].x.length; i++){
+			poly3d[patchNum].x[i] = poly3d[patchNum].x[i] + 0;
+			poly3d[patchNum].y[i] = 0;
+			poly3d[patchNum].z[i] = 0;
+		}
+			
+
+	}
+	
+	public void inflateArray(Polygon3D[] poly3d, int numOfPatches) {
+		for (int i=0; i<numOfPatches; i++)
+			inflate(poly3d, i, numOfPatches);
+	}
+	
 	
 	public static Polygon3D from2Dto3D(Polygon2D polygons2d) {
 		double[] z = new double[polygons2d.P.xpoints.length];
